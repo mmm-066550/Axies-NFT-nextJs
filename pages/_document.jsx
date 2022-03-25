92fb9aa32cb4cee1595708d6fe04bddf888a0d6c
Moustapha Mahmoud <mmm066550@gmail.com>
2022-03-25 08:33:52 +0200

    ...

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -4,7 +4,12 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link rel="stylesheet" href="/static/assets/fonts/font.css" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Urbanist:wght@100;200;300;400;500;600;700;800;900&display=optional"
+          rel="stylesheet"
+        />
       </Head>
       <body>
         <Main />
