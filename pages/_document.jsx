1d431b2455a2f569139f85f0f5b8e29cd4ee968f
Moustapha Mahmoud <mmm066550@gmail.com>
2022-03-25 08:36:08 +0200

    ...

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -10,6 +10,7 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Urbanist:wght@100;200;300;400;500;600;700;800;900&display=optional"
           rel="stylesheet"
         />
+        <link rel="stylesheet" href="/static/assets/fonts/fonts.sass" />
       </Head>
       <body>
         <Main />
