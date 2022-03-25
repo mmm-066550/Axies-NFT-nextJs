177310bff7d67400f76e874e8698aedd54db5c84
Moustapha Mahmoud <mmm066550@gmail.com>
2022-03-25 08:17:45 +0200

    fix missed fonts

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -6,10 +6,10 @@ export default function Document() {
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Urbanist:wght@100;200;300;400;500;600;700;800;900&display=optional"
+        <style
+          data-href="https://fonts.googleapis.com/css2?family=Urbanist:wght@100;200;300;400;500;600;700;800;900&display=optional"
           rel="stylesheet"
-        />
+        ></style>
       </Head>
       <body>
         <Main />
